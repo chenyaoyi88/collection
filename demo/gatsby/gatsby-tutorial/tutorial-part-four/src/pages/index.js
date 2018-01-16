c96f563b9ef55960bb3d93845c0264aaa6b98275
chenyaoyi88 <152296058@qq.com>
2018-01-16 23:37:28 +0800

    教程模版添加完成

--- demo/gatsby/gatsby-tutorial/tutorial-part-four/src/pages/index.js
+++ demo/gatsby/gatsby-tutorial/tutorial-part-four/src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import g from "glamorous";
+import Link from "gatsby-link";
 
 import { rhythm } from "../utils/typography";
 
@@ -13,11 +14,16 @@ export default ({ data }) => {
       <h4>{data.allMarkdownRemark.totalCount} 篇文章</h4>
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
-          <g.H3 marginBottom={rhythm(1 / 4)}>
-            标题：{node.frontmatter.title}{" "}
-            <g.Span color="#BBB">— 日期：{node.frontmatter.date}</g.Span>
-          </g.H3>
-          <p>摘要：{node.excerpt}</p>
+          <Link
+            to={node.fields.slug}
+            css={{ textDecoration: `none`, color: `inherit` }}
+          >
+            <g.H3 marginBottom={rhythm(1 / 4)}>
+              标题：{node.frontmatter.title}{" "}
+              <g.Span color="#BBB">— 日期：{node.frontmatter.date}</g.Span>
+            </g.H3>
+            <p>摘要：{node.excerpt}</p>
+          </Link>
         </div>
       ))}
     </div>
@@ -34,6 +40,9 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
           }
           excerpt
         }
--- demo/gatsby/gatsby-tutorial/tutorial-part-four/src/pages/md/md2.md
+++ demo/gatsby/gatsby-tutorial/tutorial-part-four/src/pages/md/md2.md
@@ -7,4 +7,5 @@ date: "2017-08-10"
 
 白斑病白斑病白斑病
 
+
 <iframe width="560" height="315" src="https://www.youtube.com/embed/4n0xNbfJLR8" frameborder="0" allowfullscreen></iframe>
