5fe3c0c67093478189adbc1dbb782f67f41ac6bc
chenyaoyi88 <152296058@qq.com>
2018-04-05 02:03:59 +0800

    首页布局

--- demo/mpvue/src/pages/a_tab/index/index.vue
+++ demo/mpvue/src/pages/a_tab/index/index.vue
@@ -43,9 +43,43 @@
 
         <div class="item">
           <div class="item-l"></div>
+          <div class="item-r no-boder-top">
+            <div class="item-r-title">
+              <span class="title">车型</span>
+            </div>
+            <div class="item-r-value">
+              <span class="value">小型面包</span>
+            </div>
+            <div class="item-r-arrow">
+              <img class="img-item-arrow" src="../../../../static/images/route-arrow.png" alt="">
+            </div>
+          </div>
+        </div>
+        
+        <div class="item">
+          <div class="item-l"></div>
           <div class="item-r">
-            <div class="item-r-title"></div>
-            <div class="item-r-value"></div>
+            <div class="item-r-title">
+              <span class="title">时间</span>
+            </div>
+            <div class="item-r-value">
+              <span class="value">立即叫车</span>
+            </div>
+            <div class="item-r-arrow">
+              <img class="img-item-arrow" src="../../../../static/images/route-arrow.png" alt="">
+            </div>
+          </div>
+        </div>
+        
+        <div class="item">
+          <div class="item-l"></div>
+          <div class="item-r">
+            <div class="item-r-title">
+              <span class="title">额外服务</span>
+            </div>
+            <div class="item-r-value">
+              <span class="light">装卸搬运等额外服务</span>
+            </div>
             <div class="item-r-arrow">
               <img class="img-item-arrow" src="../../../../static/images/route-arrow.png" alt="">
             </div>
