980430544047a340b00cf41077b52c08b1746f9e
chenyaoyi88 <152296058@qq.com>
2018-06-01 19:53:59 +0800

    临时文件

---
+++ demo/apph5-demo/src/js/common.js
@@ -0,0 +1,78 @@
+function zerofillBack(x) {
+    var f = parseFloat(x);
+    if (isNaN(f)) {
+        return false;
+    }
+    var f = Math.round(x * 100) / 100;
+    var s = f.toString();
+    var rs = s.indexOf('.');
+    if (rs < 0) {
+        rs = s.length;
+        s += '.';
+    }
+    while (s.length <= rs + 2) {
+        s += '0';
+    }
+    return s;
+}
+
+function formatNumber(n) {
+    const str = n.toString();
+    return str[1] ? str : `0${str}`;
+}
+
+function formatTime(date) {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const second = date.getSeconds();
+
+    const t1 = [year, month, day].map(formatNumber).join('.');
+    const t2 = [hour, minute, second].map(formatNumber).join(':');
+
+    return `${t1} ${t2}`;
+}
+
+//文档高度
+function getDocumentTop() {
+    var scrollTop = 0,
+        bodyScrollTop = 0,
+        documentScrollTop = 0;
+    if (document.body) {
+        bodyScrollTop = document.body.scrollTop;
+    }
+    if (document.documentElement) {
+        documentScrollTop = document.documentElement.scrollTop;
+    }
+    scrollTop = (bodyScrollTop - documentScrollTop > 0) ? bodyScrollTop : documentScrollTop;
+    return scrollTop;
+}
+
+//可视窗口高度
+function getWindowHeight() {
+    var windowHeight = 0;
+    if (document.compatMode == "CSS1Compat") {
+        windowHeight = document.documentElement.clientHeight;
+    } else {
+        windowHeight = document.body.clientHeight;
+    }
+    return windowHeight;
+}
+
+//滚动条滚动高度
+function getScrollHeight() {
+    var scrollHeight = 0,
+        bodyScrollHeight = 0,
+        documentScrollHeight = 0;
+    if (document.body) {
+        bodyScrollHeight = document.body.scrollHeight;
+    }
+    if (document.documentElement) {
+        documentScrollHeight = document.documentElement.scrollHeight;
+    }
+    scrollHeight = (bodyScrollHeight - documentScrollHeight > 0) ? bodyScrollHeight : documentScrollHeight;
+    return scrollHeight;
+}
---
+++ demo/apph5-demo/src/js/mock.js
@@ -0,0 +1,91 @@
+var mockList = [{
+    "id": 1,
+    "merchantNo": "1",
+    "remark": null,
+    "balance": 0.00,
+    "changeType": 1,
+    "changeAmount": 1.00,
+    "createTime": 1527757233000,
+    "updateTime": 1527757266000
+},{
+    "id": 1,
+    "merchantNo": "1",
+    "remark": null,
+    "balance": 0.00,
+    "changeType": 1,
+    "changeAmount": 1.00,
+    "createTime": 1527757233000,
+    "updateTime": 1527757266000
+},{
+    "id": 1,
+    "merchantNo": "1",
+    "remark": null,
+    "balance": 0.00,
+    "changeType": 1,
+    "changeAmount": 1.00,
+    "createTime": 1527757233000,
+    "updateTime": 1527757266000
+},{
+    "id": 1,
+    "merchantNo": "1",
+    "remark": null,
+    "balance": 0.00,
+    "changeType": 1,
+    "changeAmount": 1.00,
+    "createTime": 1527757233000,
+    "updateTime": 1527757266000
+},{
+    "id": 1,
+    "merchantNo": "1",
+    "remark": null,
+    "balance": 0.00,
+    "changeType": 1,
+    "changeAmount": 1.00,
+    "createTime": 1527757233000,
+    "updateTime": 1527757266000
+},{
+    "id": 1,
+    "merchantNo": "1",
+    "remark": null,
+    "balance": 0.00,
+    "changeType": 1,
+    "changeAmount": 1.00,
+    "createTime": 1527757233000,
+    "updateTime": 1527757266000
+},{
+    "id": 1,
+    "merchantNo": "1",
+    "remark": null,
+    "balance": 0.00,
+    "changeType": 1,
+    "changeAmount": 1.00,
+    "createTime": 1527757233000,
+    "updateTime": 1527757266000
+},{
+    "id": 1,
+    "merchantNo": "1",
+    "remark": null,
+    "balance": 0.00,
+    "changeType": 1,
+    "changeAmount": 1.00,
+    "createTime": 1527757233000,
+    "updateTime": 1527757266000
+},{
+    "id": 1,
+    "merchantNo": "1",
+    "remark": null,
+    "balance": 0.00,
+    "changeType": 1,
+    "changeAmount": 1.00,
+    "createTime": 1527757233000,
+    "updateTime": 1527757266000
+},{
+    "id": 1,
+    "merchantNo": "1",
+    "remark": null,
+    "balance": 0.00,
+    "changeType": 1,
+    "changeAmount": 1.00,
+    "createTime": 1527757233000,
+    "updateTime": 1527757266000
+}];
